130bad892948defc35d1c04c8849d30b76af438d
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-02-19 14:22:30 -0500

    Update subsystems and commands doc

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Increment when you update the repo
-const cacheName = "KnowledgeBase-v11";
+const cacheName = "KnowledgeBase-v12";
 // URLs for all the assets
 const resourceFiles = [
 	// Just The Docs assets
